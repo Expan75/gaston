8852b9111fcfff4ce01ecd92f1ad1b35baa91075
Erik Håkansson <erik.hakansson@softhouse.se>
2021-12-15 19:00:07 +0100

    init commit /w postgres n config

---
+++ api/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'test', 'production')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }

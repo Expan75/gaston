af7dd702c138f60d48b39b49ca8e9c5fc4e352d1
hakan <erik.hakansson96@gmail.com>
2022-05-18 20:33:14 +0200

    remade into ts-rn /w expo

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import RootNavigator from './src/navigation/RootNavigator';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-const App = () => {
-  return <RootNavigator />;
-};
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
-export default App;
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}

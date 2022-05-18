9ef4df3cac9046c60f38ff635a6ea5efcdf72952
hakan <erik.hakansson96@gmail.com>
2022-05-19 00:31:05 +0200

    testing location library out

--- app/screens/TabOneScreen.tsx
+++ app/screens/TabOneScreen.tsx
@@ -1,13 +1,34 @@
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [location, setLocation] = useState(null);
+  const [locationError, setLocationError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if ( status !== 'granted' ) {
+        setLocationError('Permission to access location was deined.');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, [])
+
+  let text = locationError ? locationError : JSON.stringify(location);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>{text}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
